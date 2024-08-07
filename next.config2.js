e18293b9fa2c49ae18ed12791710a8bad6e2e7c1
jobx10 <jobx10mx@gmail.com>
2024-08-07 11:26:12 +0800

    Update next.config2.js

--- next.config2.js
+++ next.config2.js
@@ -2,7 +2,7 @@
 module.exports = {
   env: {
     output: "export",  // <=== enables static exports
-    basePath: "/nextjs_3_2crud_sqlite",
+  
     CSRF_SECRET : 'secret1234',
     reactStrictMode: true,
    
